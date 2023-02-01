c2dc6dd1259f438cc624535becf39bf7af1a06e4
Karina Narkeviciute <karina.narkeviciute@gmail.com>
2023-02-01 12:20:52 +0100

    fetch data from API

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { LinearProgress } from "@mui/material";
+import { useState } from "react";
+import { useQuery } from "react-query";
+
+// Components
+
+// Styles
+import { Wrapper } from "./App.styles";
+
+// Types
+export type CartItemType = {
+  id: number;
+  category: string;
+  description: string;
+  image: string;
+  price: number;
+  title: string;
+  amount: number;
+};
+
+// const getProducts = async (): Promise<CartItemType> => {
+//   await (await fetch("https://fakestoreapi.com/products")).json();
+// };
+
+const getProducts = (): Promise<CartItemType[]> => {
+  return fetch("https://fakestoreapi.com/products")
+    .then((res) => {
+      return res.json();
+    })
+    .catch((err) => console.log(err));
+};
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+  const { data, isLoading, error } = useQuery<CartItemType[]>("products", getProducts);
 
+  const getTotalItems = () => null;
+  const handleAddToCart = () => null;
+  const handleRemoveFromCart = () => null;
+
+  if (isLoading) return <LinearProgress />;
+  if (error) return <div>Something went wrong...</div>;
+
+  return <div className="App">My App</div>;
+}
 export default App;
