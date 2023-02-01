e799c3c33ea52be6fcd6f858bc2d438201c441b0
Karina Narkeviciute <karina.narkeviciute@gmail.com>
2023-02-01 14:39:20 +0100

    implement  functionality add/remove from cart

--- src/App.tsx
+++ src/App.tsx
@@ -47,9 +47,33 @@ function App() {
     return items.reduce((acc: number, item) => acc + item.amount, 0);
   };
 
-  const handleAddToCart = (clickedItem: CartItemType) => null;
+  const handleAddToCart = (clickedItem: CartItemType) => {
+    setCartItems((prev) => {
+      const isItemInCart = prev.find((item) => item.id === clickedItem.id);
 
-  const handleRemoveFromCart = () => null;
+      if (isItemInCart) {
+        return prev.map((item) => (item.id === clickedItem.id ? { ...item, amount: item.amount + 1 } : item));
+      } else {
+        return [...prev, { ...clickedItem, amount: 1 }];
+      }
+    });
+  };
+
+  const handleRemoveFromCart = (id: number) => {
+    setCartItems((prev) => {
+      return prev.reduce((acc, item) => {
+        if (item.id === id) {
+          if (item.amount === 1) {
+            return acc;
+          } else {
+            return [...acc, { ...item, amount: item.amount - 1 }];
+          }
+        } else {
+          return [...acc, item];
+        }
+      }, [] as CartItemType[]);
+    });
+  };
 
   if (isLoading) return <LinearProgress />;
   if (error) return <div>Something went wrong...</div>;
--- src/components/Cart/Cart.tsx
+++ src/components/Cart/Cart.tsx
@@ -9,15 +9,20 @@ type CartProps = {
 };
 
 const Cart: React.FC<CartProps> = ({ cartItems, addToCart, removeFromCart }) => {
+  const calculateTotal = (items: CartItemType[]) => {
+    return items.reduce((acc: number, item) => {
+      return acc + item.amount * item.price;
+    }, 0);
+  };
+
   return (
     <Wrapper>
-      <>
-        <h2>Your Shopping Cart</h2>
-        {cartItems.length === 0 ? <p>No items in cart</p> : null}
-        {cartItems.map((item) => {
-          return <CartItem key={item.id} item={item} addToCart={addToCart} removeFromCart={removeFromCart} />;
-        })}
-      </>
+      <h2>Your Shopping Cart</h2>
+      {cartItems.length === 0 ? <p>No items in cart</p> : null}
+      {cartItems.map((item) => {
+        return <CartItem key={item.id} item={item} addToCart={addToCart} removeFromCart={removeFromCart} />;
+      })}
+      <h2>Total: ${calculateTotal(cartItems).toFixed(2)}</h2>
     </Wrapper>
   );
 };
